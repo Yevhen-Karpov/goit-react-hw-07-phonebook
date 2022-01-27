aa95a51cf2415ecff4e72475b2b9d9be80df767f
Yevhen-Karpov <goldblogart@gmail.com>
2022-01-27 13:26:05 +0200

    1

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,29 +1,8 @@
 import { createAction } from '@reduxjs/toolkit';
 
-const getContactRequest = createAction('contacts/getContactRequest');
-const getContactSuccess = createAction('contacts/getContactSuccess');
-const getContactError = createAction('contacts/getContactError');
-
-const addContactRequest = createAction('contacts/addContactRequest');
-const addContactSuccess = createAction('contacts/addContactSuccess');
-const addContactError = createAction('contacts/addContactError');
-
-const deleteContactRequest = createAction('contacts/deleteContactRequest');
-const deleteContactSuccess = createAction('contacts/deleteContactSuccess');
-const deleteContactError = createAction('contacts/deleteContactError');
-
 const changeFilter = createAction('contacts/changeFilter');
 
 const actions = {
-  getContactRequest,
-  getContactSuccess,
-  getContactError,
-  addContactRequest,
-  addContactSuccess,
-  addContactError,
-  deleteContactRequest,
-  deleteContactSuccess,
-  deleteContactError,
   changeFilter,
 };
 
