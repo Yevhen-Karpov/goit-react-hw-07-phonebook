ea1d4ec5fcad961ff3f219341d481b3bd033bec5
Yevhen-Karpov <goldblogart@gmail.com>
2022-01-27 13:41:01 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,20 +1,41 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import * as contactsApi from '../services/contactsFetch-api';
 
-const getALLContacts = createAsyncThunk('contacts/getALLContacts', async () => {
-  const contacts = await contactsApi.fetchContacts();
-  return contacts;
-});
+const getALLContacts = createAsyncThunk(
+  'contacts/getALLContacts',
+  async (_, { rejectWithValue }) => {
+    try {
+      const contacts = await contactsApi.fetchContacts();
+      return contacts;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
 
-const addContact = createAsyncThunk('contacts/addContacts', async payload => {
-  const contacts = await contactsApi.fetchAddContact(payload);
-  return contacts;
-});
+const addContact = createAsyncThunk(
+  'contacts/addContacts',
+  async (payload, { rejectWithValue }) => {
+    try {
+      const contacts = await contactsApi.fetchAddContact(payload);
+      return contacts;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
 
-const deleteContact = createAsyncThunk('contacts/deleteContact', async id => {
-  const contacts = await contactsApi.fetchDeleteContact(id);
-  return contacts;
-});
+const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, { rejectWithValue }) => {
+    try {
+      const contacts = await contactsApi.fetchDeleteContact(id);
+      return contacts;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
 
 const contactsOperations = { getALLContacts, addContact, deleteContact };
 
